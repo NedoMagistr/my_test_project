b9ba73bd2130af31be6cf1483c284e4923870d42
NedoMagistr <osroma98@gmail.com>
2021-07-08 18:37:22 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -75,30 +75,41 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                  Image(
+                Padding(
+                padding: EdgeInsets.all(1.0),
+                child:Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                    Image(
                       image: NetworkImage(userData[index].picture),
                       width: 150.0,
                       height: 100.0,
-                  ),
-                   Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                      ),
+                    Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("${userData[index].name} ",
+                      Text("${userData[index].name} ",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                        child: Text(" ${userData[index].description}",
                           style: TextStyle(
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Text(" ${userData[index].description}",
-                            style: TextStyle(
                             fontSize: 20.0,
-                            ),
                           ),
                         ),
-                      ],
+                      ),
+                    ],
                   ),
+                    ),
+                  ]
+                ),
+                ),
                 Text("${userData[index].id.toString()} ",
                   style: TextStyle(
                     fontSize: 20.0,
